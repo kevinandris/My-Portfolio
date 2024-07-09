5505c7c7d78e47bb0141c5bd48816f28f15f985d
kevinandris <kevinandris27@gmail.com>
2024-07-10 10:23:42 +1200

    updated the hero section

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
 
         <div className="absolute bottom-5 sm:bottom-14 left-0 sm:-left-10 mb-20 animate-bounce">
           <MovingBorderBtn borderRadius="0.5rem" className="p-3 font-semibold">
-            <p> 💻 Looking for an internship or entry-level position </p>
+            <p> 💻 Available for an internship or entry-level position </p>
           </MovingBorderBtn>
         </div>
       </div>
